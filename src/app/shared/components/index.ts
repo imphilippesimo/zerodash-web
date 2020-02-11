946a643a64907f4b9fe5252752434c5f5c7e6a59
philippe SIMO <philippechampion58@gmail.com>
2020-02-11 11:32:58 +0100

    initial commit

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,5 @@
+import {CallToActionComponent} from './call-to-action/call-to-action.component';
+
+export const components: any[] = [CallToActionComponent];
+
+export * from './call-to-action/call-to-action.component';
